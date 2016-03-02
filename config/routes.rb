5da9f520311c19be3fa03c6c2fb49cb68c46505a
Henry Jenkins <henry@henryjenkins.name>
2016-03-01 23:17:49 -0500

    Wrote quick home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :artists, only: [:index, :show]
   resources :events, only: [:index, :show]
   resources :venues, only: [:index, :show]
+
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest
   # priority. See how all your routes lay out with "rake routes".
 
