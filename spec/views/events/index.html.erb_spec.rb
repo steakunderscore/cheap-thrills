10d7d32fae377bd6de3375fd9b387276cf22f8da
Henry Jenkins <henry@henryjenkins.name>
2016-03-03 11:05:32 -0500

    Fixed up event tests

--- spec/views/events/index.html.erb_spec.rb
+++ spec/views/events/index.html.erb_spec.rb
@@ -3,23 +3,13 @@ require "rails_helper"
 RSpec.describe "events/index", type: :view do
   before(:each) do
     assign(:events, [
-             Event.create!(
-               venue: nil,
-               price: 1,
-               soldout: false
-             ),
-             Event.create!(
-               venue: nil,
-               price: 1,
-               soldout: false
-             )
+             FactoryGirl.create(:event),
+             FactoryGirl.create(:event)
            ])
   end
 
   it "renders a list of events" do
     render
-    assert_select "tr>td", text: nil.to_s, count: 2
-    assert_select "tr>td", text: 1.to_s, count: 2
-    assert_select "tr>td", text: false.to_s, count: 2
+    assert_select "tbody>tr", count: 2
   end
 end
--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -2,17 +2,14 @@ require "rails_helper"
 
 RSpec.describe "events/show", type: :view do
   before(:each) do
-    @event = assign(:event, Event.create!(
-                              venue: nil,
-                              price: 1,
-                              soldout: false
-    ))
+    @event = assign(:event, FactoryGirl.create(:event, price: 1000, soldout:
+                                               false))
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(//)
-    expect(rendered).to match(/1/)
-    expect(rendered).to match(/false/)
+    expect(rendered).to match(/\$10.0/)
+    expect(rendered).to match(/Not yet/)
   end
 end
