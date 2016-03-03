6ae7799d2eed875a9e0f8e7f2e95ce78363d9dbe
Henry Jenkins <henry@henryjenkins.name>
2016-03-03 09:12:10 -0500

    Added tests for the artist views

--- spec/views/artists/index.html.erb_spec.rb
+++ spec/views/artists/index.html.erb_spec.rb
@@ -3,20 +3,13 @@ require "rails_helper"
 RSpec.describe "artists/index", type: :view do
   before(:each) do
     assign(:artists, [
-             Artist.create!(
-               event: nil,
-               name: "Name"
-             ),
-             Artist.create!(
-               event: nil,
-               name: "Name"
-             )
+             FactoryGirl.create(:artist, name: "Name A"),
+             FactoryGirl.create(:artist, name: "Name B")
            ])
   end
 
   it "renders a list of artists" do
     render
-    assert_select "tr>td", text: nil.to_s, count: 2
-    assert_select "tr>td", text: "Name".to_s, count: 2
+    assert_select "tr>td", /^Name (A|B)/, count: 2
   end
 end
--- spec/views/artists/show.html.erb_spec.rb
+++ spec/views/artists/show.html.erb_spec.rb
@@ -2,15 +2,17 @@ require "rails_helper"
 
 RSpec.describe "artists/show", type: :view do
   before(:each) do
-    @artist = assign(:artist, Artist.create!(
-                                event: nil,
-                                name: "Name"
-    ))
+    @artist = assign(:artist, FactoryGirl.create(:artist, name: "Name"))
   end
 
-  it "renders attributes in <p>" do
+  it "renders artist name in <h1>" do
     render
-    expect(rendered).to match(//)
-    expect(rendered).to match(/Name/)
+    assert_select "h1", text: "Name", count: 1
+  end
+
+  it "renders the upcoming partial" do
+    render
+    expect(response).to render_template(partial:
+                                        "application/_upcoming_events")
   end
 end
