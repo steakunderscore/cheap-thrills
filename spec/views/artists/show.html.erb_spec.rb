dd11d321b62815771c0e77bf6ff117b4d50ce73f
Henry Jenkins <henry@henryjenkins.name>
2016-03-06 23:30:24 -0500

    Added test and fix time rendering bug

--- spec/views/artists/show.html.erb_spec.rb
+++ spec/views/artists/show.html.erb_spec.rb
@@ -2,7 +2,13 @@ require "rails_helper"
 
 RSpec.describe "artists/show", type: :view do
   before(:each) do
-    @artist = assign(:artist, FactoryGirl.create(:artist, name: "Name"))
+    @artist = FactoryGirl.create(:artist, name: "Name")
+    march6at10pm = Time.new(4016, 03, 06, 22, 43, 00, "-05:00")
+    @artist.events << FactoryGirl.create(:event,
+                                         starts_at: march6at10pm)
+
+    assign(:artist, @artist)
+    assign(:upcoming_events, @artist.events.upcoming)
   end
 
   it "renders artist name in <h1>" do
@@ -14,5 +20,10 @@ RSpec.describe "artists/show", type: :view do
     render
     expect(response).to render_template(partial:
                                         "application/_upcoming_events")
+  end
+
+  it "renders the time in localtime" do
+    render
+    expect(response).to match(/March 06, 4016 22:43/)
   end
 end
